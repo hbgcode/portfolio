d78a3e778d5cf0453255b0ca25d49320cec22c77
hbgcode <hemaborgohain.123@gmail.com>
2023-07-07 20:57:34 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/about/About";
-import Intro from "./components/introduction/intro";
-import ProductList from "./components/productlist/ProductList";
+import Intro from "./components/introduction/Intro";
+import ProductList from "./components/ProductList/ProductList";
 import Contact from "./contact/Contact";
 
 const App = () => {
